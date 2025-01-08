95ad456f3a8c1acad27f49bf44969a7ef3018b5a
ReonaRea <celiaclenn@gmail.com>
2025-01-08 12:48:10 +1030

    update asset prefix and base path

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "docs",
-  basePath: "/babayaga",
+  assetPrefix: isProd ? "/babayaga/" : "",
+  basePath: isProd ? "/babayaga" : "",
 };
 
 export default nextConfig;
