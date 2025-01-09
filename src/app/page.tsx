d28493814ba8ecf4143ca7aab5d8aba567463291
ReonaRea <celiaclenn@gmail.com>
2025-01-09 16:01:45 +1030

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "public/gamelogo.png";
+import Image from "next/image";
 
 const WebsiteTemplate = () => {
   return (
@@ -42,8 +42,8 @@ const WebsiteTemplate = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
           {/* Hero Section */}
           <div className="text-center">
-            <img
-              src={logo}
+            <Image
+              src="/gamelogo.png"
               alt="Company Logo"
               className="h-8 w-auto"
               loading="eager"
